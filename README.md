05bc3695ab2cf59f8d6785aa5c433aa68a9ac25e
Pablo-Severino <pabloseverino40@gmail.com>
2023-01-21 08:27:02 -0300

    Aula 04 - React Native

---
+++ README.md
@@ -0,0 +1,6 @@
+<h2 align="center">NLW Setup</h2>
+<p align="center">A nlw é um evento organizado pela Rocketseat que contem 5 aulas de alta qualidade.</p>
+
+
+<img src="1920x1080-Green.png" />
+<img src="1920x1080-Purple.png" />
--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -1,9 +1,9 @@
-import { ScrollView, Text, View } from "react-native";
 import { useRoute } from "@react-navigation/native";
+import { ScrollView, Text, View } from "react-native";
 import dayjs from "dayjs";
 
 import { BackButton } from "../components/BackButton";
-import { ProgressBar } from "../components/ProgressBar";
+import { ProgressBar } from "../components/Progress.Bar";
 import { Checkbox } from "../components/Checkbox";
 
 interface Params {
@@ -11,12 +11,12 @@ interface Params {
 }
 
 export function Habit() {
-  const route = useRoute();
+  const route = useRoute()
   const { date } = route.params as Params;
 
   const parsedDate = dayjs(date);
-  const dayOfWeek = parsedDate.format("dddd");
-  const dayAndMonth = parsedDate.format("DD/MM");
+  const dayOfWeek = parsedDate.format('dddd');
+  const dayAndMonth = parsedDate.format('DD/MM');
 
   return (
     <View className="flex-1 bg-background px-8 pt-16">
@@ -25,25 +25,29 @@ export function Habit() {
         contentContainerStyle={{ paddingBottom: 100 }}
       >
         <BackButton />
+
         <Text className="mt-6 text-zinc-400 font-semibold text-base lowercase">
           {dayOfWeek}
         </Text>
+
         <Text className="text-white font-extrabold text-3xl">
           {dayAndMonth}
         </Text>
+
         <ProgressBar progress={30} />
+
         <View className="mt-6">
-          <Checkbox
+          <Checkbox 
             title="Beber 2L de água"
             checked={false}
           />
 
-          <Checkbox
+          <Checkbox 
             title="Caminhar"
-            checked={true}
+            checked
           />
         </View>
       </ScrollView>
     </View>
-  );
+  )
 }
