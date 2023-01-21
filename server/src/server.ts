cf099056aac8bc0d071d68fb51c2ccc186aaabda
Pablo-Severino <pabloseverino40@gmail.com>
2023-01-21 18:36:31 -0300

    Aula 05 - Node JS

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,15 +9,8 @@ app.register(cors);
 app.register(appRoutes);
 
 app.listen({
-  host: "0.0.0.0",
   port: 3333,
 })
-.then(async () => {
-  const tunnel = await localtunnel({
-    local_host: "0.0.0.0",
-    port: 3333,
-    subdomain: "habits-api",
-  });
-
+.then(() => {
   console.log("HTTP Server Running!");
 });
